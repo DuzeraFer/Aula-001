f249abb3865b3c2c5a79bd1ddafa1cc4c03272ff
DuzeraFer <duardo.fernandessbr@gmail.com>
2022-03-24 12:15:30 -0300

    Adicionado sistema de quebra de cenário
    Adicionado objetos quebráveis com tiros, sendo 3 paredes e 1 barco.

--- Assets/Scripts/ControleBala.cs
+++ Assets/Scripts/ControleBala.cs
@@ -7,29 +7,10 @@ public class ControleBala : MonoBehaviour
     Rigidbody rgbdBala;
     public float speed;
 
-    ParedeQuebra paredeQuebraScript;
-
     // Start is called before the first frame update
     void Start()
     {
         rgbdBala = GetComponent<Rigidbody>();
         rgbdBala.AddForce(transform.forward * speed, ForceMode.Impulse);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "ParedeQuebra")
-        {
-            paredeQuebraScript = collision.gameObject.GetComponent<ParedeQuebra>();
-            paredeQuebraScript.LevarDano();
-        }
-
-        Destroy(gameObject);
     }
 }
